7c601259b6ea5a890dc66e373b227682d21d1776
GustavoTJS <gustavo.gomes080108@gmail.com>
2024-10-29 01:43:10 -0300

    Aula PHP e exercício de Ímpar ou Par

--- aula 06/atividade.php
+++ aula 06/atividade.php
@@ -4,7 +4,7 @@ $parOuImpar = array(2, 5, 7, 8, 10, 18, 30, 37, 38, 60);
 
 for ($i = 0; $i < count ($parOuImpar); $i++) {
     if ($parOuImpar[$i] % 2 == 0)
-    echo "Número Par: $par_ou_impar[$i] <br />";
+    echo "Número Par: $parOuImpar[$i]";
     else 
-    echo "Número Impar:  $par_ou_impar[$i] <br />";
+    echo "Número Impar:  $parOuImpar[$i]";
 }
